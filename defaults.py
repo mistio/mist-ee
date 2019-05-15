5b5f2ba99d86bbae805cccd291231ceb3624754b
Dimitris Moraitis <dimo@mist.io>
2019-05-16 02:36:49 +0300

    Update portal name, logo, currency rate, api & ui suhmodules

--- defaults.py
+++ defaults.py
@@ -1,17 +1,18 @@
 """
-Default configuration options for Computrade Panel
+Default configuration options for CTI Cloud Manager (CCM)
 """
-PORTAL_NAME = "Computrade Panel"
+PORTAL_NAME = "CTI Cloud Manager"
+EMAIL_LOGO = "static/CTI-logo2.png"
 THEME = "computrade"
-EMAIL_FROM = "Computrade Panel <panel@computradetech.com>"
+EMAIL_FROM = "CTI Cloud Manager <cti@computradetech.com>"
 EMAIL_ALERTS = "alert@computradetech.com"
 EMAIL_INFO = "info@computradetech.com"
 EMAIL_SALES = "sales@computradetech.com"
 EMAIL_SUPPORT = "support@computradetech.com"
-EMAIL_NOTIFICATIONS = "notifications@clear.glass"
+EMAIL_NOTIFICATIONS = "notifications@computradetech.com"
 CURRENCY = {
     "sign": "Rp",
-    "rate": 14028.0
+    "rate": 14430.0
 }
 ENABLE_MONITORING = True
 DOCS_URI = SUPPORT_URI = ''
@@ -24,36 +25,18 @@ PLUGINS = [
     'auth',
 ]
 
-LANDING_CATEGORIES = [{
-    'href': '/',
-    'name': 'home',
-    'template': 'home',
-    'title': 'Home',
-    'items': {
-        "fold": {
-            "copy" : "Computrade Panel",
-            "subcopy" :
-                "A single dashboard to manage multi-cloud infrastructure",
-            "image" : "",
-            "alt" : "Computrade Panel cloud management dashboard",
-            "cta" : "Sign Up"
-        }
-    }
-}]
-
-
 EC2_SECURITYGROUP = {
-    'name': 'computrade-panel',
-    'description': 'Security group created by Computrade Panel'
+    'name': 'cti-cloud-manager',
+    'description': 'Security group created by CTI Cloud Manager (CCM)'
 }
 
 
-CONFIRMATION_EMAIL_SUBJECT = "[Computrade Panel] Confirm your registration"
+CONFIRMATION_EMAIL_SUBJECT = "[CTI Cloud Manager (CCM)] Confirm your registration"
 
 CONFIRMATION_EMAIL_BODY = \
 """Hi %s,
 
-we received a registration request to Computrade Panel from this email address.
+we received a registration request to CTI Cloud Manager from this email address.
 
 To activate your account, please click on the following link:
 
@@ -63,14 +46,14 @@ This request originated from the IP address %s. If it wasn't you, simply ignore
 this message.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
 """
 
 
-RESET_PASSWORD_EMAIL_SUBJECT = "[Computrade Panel] Password reset request"
+RESET_PASSWORD_EMAIL_SUBJECT = "[CTI Cloud Manager (CCM)] Password reset request"
 
 RESET_PASSWORD_EMAIL_BODY = \
 """Hi %s,
@@ -85,14 +68,14 @@ this message. Your password has not been changed.
 
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
 """
 
 
-WHITELIST_IP_EMAIL_SUBJECT = "[Computrade Panel] Account IP whitelist request"
+WHITELIST_IP_EMAIL_SUBJECT = "[CTI Cloud Manager (CCM)] Account IP whitelist request"
 
 WHITELIST_IP_EMAIL_BODY = \
 """Hi %s,
@@ -107,7 +90,7 @@ this message. The above IP will not be whitelisted.
 
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
@@ -115,10 +98,10 @@ Govern the clouds
 """
 
 
-FAILED_LOGIN_ATTEMPTS_EMAIL_SUBJECT = "[Computrade Panel] Failed login attempts warning"
+FAILED_LOGIN_ATTEMPTS_EMAIL_SUBJECT = "[CTI Cloud Manager (CCM)] Failed login attempts warning"
 
 
-ORG_NOTIFICATION_EMAIL_SUBJECT = "[Computrade Panel] Subscribed to team"
+ORG_NOTIFICATION_EMAIL_SUBJECT = "[CTI Cloud Manager (CCM)] Subscribed to team"
 
 USER_NOTIFY_ORG_TEAM_ADDITION = \
 """Hi
@@ -126,7 +109,7 @@ USER_NOTIFY_ORG_TEAM_ADDITION = \
 You have been added to the team "%s" of organization %s.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
@@ -143,17 +126,17 @@ To confirm your invitation, please click on the following link:
 %s/confirm-invitation?invitoken=%s
 
 Once you are done with the confirmation process,
-you will be able to login to your Computrade Panel user account
+you will be able to login to your CTI Cloud Manager user account
 as a member of the team%s.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
 """
 
-ORG_INVITATION_EMAIL_SUBJECT = "[Computrade Panel] Confirm your invitation"
+ORG_INVITATION_EMAIL_SUBJECT = "[CTI Cloud Manager (CCM)] Confirm your invitation"
 
 REGISTRATION_AND_ORG_INVITATION_EMAIL_BODY = \
 """Hi
@@ -161,18 +144,18 @@ REGISTRATION_AND_ORG_INVITATION_EMAIL_BODY = \
 You have been invited by %s to join the %s organization
 as a member of the %s.
 
-Before joining the team you must also activate your account in  Computrade Panel and set
+Before joining the team you must also activate your account in  CTI Cloud Manager and set
 a password. To activate your account and join the team, please click on the
 following link:
 
 %s/confirm?key=%s&invitoken=%s
 
 Once you are done with the registration process,
-you will be able to login to your Computrade Panel user account
+you will be able to login to your CTI Cloud Manager user account
 as a member of the team%s.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
@@ -185,7 +168,7 @@ You have been removed from team %s of organization %s by the
 administrator %s.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
@@ -197,7 +180,7 @@ NOTIFY_REMOVED_FROM_ORG = \
 You are no longer a member of the organization %s.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
@@ -211,7 +194,7 @@ NOTIFY_INVITATION_REVOKED = \
 Your invitation to the organization %s has been revoked.
 
 Best regards,
-The Computrade Panel team
+The CTI Cloud Manager team
 
 --
 %s
